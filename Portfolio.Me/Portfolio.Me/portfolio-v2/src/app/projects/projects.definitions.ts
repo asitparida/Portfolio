2f6f2aea98efb227885e0b3812d7569ae834a5f5
Asit Parida <asitparida@live.in>
2017-12-09 19:18:49 +0530

    Add project description windows

---
+++ Portfolio.Me/Portfolio.Me/portfolio-v2/src/app/projects/projects.definitions.ts
@@ -0,0 +1,289 @@
+// tslint:disable:max-line-length
+export const LEANCASE = {
+    name: 'Lean-Case',
+    description: 'a fintech app to plan and model day-to-day business operations and revenue subscriptions',
+    git: null,
+    img: 'assets/project-logos/quickref.png',
+    href: 'http://www.lean-case.com/',
+    width: 'auto',
+    height: '50%',
+    bgColor: '$TURQUOISE',
+    id: 'pro_999',
+    publishSrc: 'http://www.lean-case.com/',
+    githubSrc: null,
+    content:
+    `
+    <p><strong>Contribution</strong></p>
+    <ul>
+      <li>Serving as freelance consultant for the project wherein I am helping redesign the experience with modern UX</li>
+      <li>Contributing to the development process as a design/front-end developer</li>
+    </ul>
+    <p><strong>Description</strong></p>
+    <p>A fin-tech start-up, Lean-Case, is a business case portal for organizations to connect with&nbsp;customers to create, manage and track the right plans and gain insights. The SAAS tool allows us to model revenues mix and provides flexible cost planning tools. With an array of templates to choose from like Subscription B2B/B2C, eCommerce, Marketplace, Advertising, License, Professional Service, etc., the application helps understand the unit economics of a customer contract and a Sales Team and&nbsp;the viability of the business model.</p>
+    <p><strong>Notes</strong></p>
+    <ul>
+      <li>Angular 2.x/4.x + TypeScript + SASS</li>
+      <li>MongoDB, RabbitMQ, OAuth 2.0, AWS</li>
+    </ul>
+    `
+};
+export const Projects = [
+    {
+        name: 'Quick References',
+        description: 'blog for tl;dr snips on tech stacks',
+        git: 'https://github.com/asitparida/QuickReferences',
+        img: 'assets/project-logos/quickref.png',
+        href: 'http://quick-ref.azurewebsites.net',
+        width: 'auto',
+        height: '50%',
+        bgColor: '$PETER_RIVER',
+        content:
+        `
+        <p><strong>Contribution</strong></p>
+        <ul>
+          <li>Designed &amp; developed the working application</li>
+        </ul>
+        <p><strong>Description</strong></p>
+        <p>QuickReferences is a tl;dr styled blog intended for quick reads on various topics related to the web development process. Topics range from core JavaScript topics like closures, compositions, currying, web fundamentals like prototypal inheritance to implementations for problems like babylonian method to calculate square-roots and look-n-say sequences. The application features reading playlists curated on similar topics. Almost every article includes a working fiddle link. The blog has been built on top of ASP.Net and uses out of box features from Azure like CosmosDB.&nbsp;</p>
+        <p><strong>Notes</strong></p>
+        <ul>
+          <li>Angular 1.x/2.x + TypeScript + SASS</li>
+          <li>React + Redux &nbsp;+ TypeScript + SASS</li>
+          <li>Azure CosmosDB, Azure Web Apps</li>
+          <li>ASP .NET Core 2.x + Visual Studio for MacOS</li>
+        </ul>
+        `
+    },
+    {
+        name: 'Reddit',
+        description: 'Concept illustration for what Reddit may look like',
+        git: 'https://github.com/asitparida/redesign-reddit',
+        img: 'assets/project-logos/reddit.png',
+        href: 'http://redesign-reddit.azurewebsites.net/',
+        width: '80%',
+        height: 'auto',
+        bgColor: '$ALIZARIN',
+        content:
+        `
+        <p><strong>Contribution</strong></p>
+        <ul>
+          <li>Developed the working prototype based on a redesign by Dmitriy Kharaberyush's @ <a rel="nofollow" href="https://dribbble.com/shots/3257279-Reddit-Trainy/attachments/699302">https://dribbble.com/shots/3257279-Reddit-Trainy/attachments/699302</a></li>
+        </ul>
+        <p><strong>Description</strong></p>
+        <p>The project tries to implement a new Reddit look-n-feel while addressing balanced web performance and modern UI guidelines and faster searching &amp; grouping. The focus was on getting the animations leaner but effective and hero virtual content loading technique in JavaScript. In addition, the prototype features easy navigation between subreddits.</p>
+        <p><strong>Notes</strong></p>
+        <ul>
+          <li>Angular 2.x/4.x + TypeScript + SASS</li>
+          <li>Azure Web App + Visual Studio Code</li>
+        </ul>
+        `
+    },
+    {
+        name: 'Feeder',
+        description: 'chrome extension for web notifications like github, mail, calendar etc',
+        git: 'https://github.com/asitparida/Now',
+        img: 'assets/project-logos/now.png',
+        href: 'https://github.com/asitparida/Now',
+        width: '90%',
+        height: 'auto',
+        bgColor: '$PETER_RIVER',
+        content:
+        `
+        <p><strong>Contribution</strong></p>
+        <ul>
+          <li>Designed and developed the working prototype</li>
+        </ul>
+        <p><strong>Description</strong></p>
+        <p>The app is inspired by the Android application Google Now. Its a browser extension for Google Chrome, titled Feeder, which accumulates actionable data from different services like emails, calendar, twitter, etc. and presents us with a personalised tab when we open a new tab in the Chrome browser. Currently under development, the idea is to continuously integrate with Bing's cognitive APIs to leverage customised insights for a user.&nbsp;</p>
+        <p><strong>Notes</strong></p>
+        <ul>
+          <li>React + Redux + Redux Observable + TypeScript + SASS</li>
+          <li>Azure Web App + Visual Studio Code</li>
+        </ul>
+        `
+    },
+    {
+        name: 'Not So Grey',
+        description: 'A desktop app for folks who dream colors',
+        git: 'https://github.com/asitparida/NotSoGrey',
+        img: 'assets/project-logos/notsogrey_app.png',
+        href: 'https://github.com/asitparida/NotSoGrey',
+        size: 'double',
+        width: '90%',
+        height: 'auto',
+        bgColor: '$NEPHRITIS'
+    },
+    {
+        name: 'Multihost Adapter',
+        description: 'An adapter for running multiple ui client frameworks together',
+        git: 'https://github.com/asitparida/Multihost.Adapter',
+        img: 'assets/project-logos/multihost.png',
+        href: 'https://github.com/asitparida/Multihost.Adapter',
+        size: 'double',
+        width: '100%',
+        height: 'auto',
+        bgColor: '$PETER_RIVER'
+    },
+    {
+        name: 'Flintstones',
+        description: 'An small animation on flintstones chracters using GSAP',
+        git: 'https://github.com/asitparida/GSAP.FlintStory',
+        img: 'assets/project-logos/flintstones.png',
+        href: 'http://gsap-flintstones-asparida.azurewebsites.net/',
+        size: 'double',
+        width: 'auto',
+        height: '90%',
+        bgColor: '$SUNFLOWER'
+    },
+    {
+        name: 'Svg Flow Box',
+        description: 'Lightweight SVG based event highlighting on flows',
+        git: 'https://github.com/asitparida/SVGFlowBox',
+        img: 'assets/project-logos/svg-flow.png',
+        href: 'https://svg-flow-box.azurewebsites.net',
+        width: '100%',
+        height: 'auto',
+        bgColor: '$PETER_RIVER'
+    },
+    {
+        name: 'Light Wizard ',
+        description: 'A light weight angular2 wizard',
+        git: 'https://github.com/asitparida/LightWizard',
+        img: 'assets/project-logos/light-wizard2.png',
+        href: 'http://light-wizard.azurewebsites.net/',
+        width: '80%',
+        height: 'auto',
+        bgColor: '$ALIZARIN'
+    },
+    {
+        name: 'ng-grid',
+        description: 'angular 2 implementation for vx-grid',
+        git: 'https://github.com/asitparida/ng-vxgrid',
+        img: 'assets/project-logos/grid_app.png',
+        href: 'https://vxgrid-ng2.azurewebsites.net/home',
+        width: '90%',
+        height: 'auto',
+        bgColor: '$PETER_RIVER'
+    },
+    {
+        name: 'Star Wars',
+        description: 'An small game built to destory all death stars',
+        git: 'https://github.com/asitparida/StarWars.Game',
+        img: 'assets/project-logos/starwars.png',
+        href: 'http://star-wars-asparida.azurewebsites.net',
+        size: 'double',
+        width: '70%',
+        height: 'auto',
+        bgColor: '$SUNFLOWER'
+    },
+    {
+        name: 'Moosik',
+        description: 'An offline music player using Electron',
+        git: 'https://github.com/asitparida/Moosik',
+        img: 'assets/project-logos/moosik_app.png',
+        href: 'https://github.com/asitparida/Moosik/releases',
+        size: 'double',
+        width: '80%',
+        height: 'auto',
+        bgColor: '$PETER_RIVER'
+    },
+    {
+        name: 'TopGit',
+        description: 'Browse top projects on GitHub',
+        git: 'https://github.com/asitparida/TopGit',
+        img: 'assets/project-logos/topgit_app.png',
+        href: 'http://topgit.azurewebsites.net',
+        width: '90%',
+        height: 'auto',
+        bgColor: '$NEPHRITIS'
+    },
+    {
+        name: 'CSS Loaders',
+        description: 'Robot like css loader animation',
+        git: 'https://github.com/asitparida/CSSRobotLoader',
+        img: 'assets/project-logos/cssloader.png',
+        href: 'http://css-loader-robot.azurewebsites.net',
+        width: 'auto',
+        height: '70%',
+        bgColor: '$PUMPKIN'
+    },
+    {
+        name: 'Tweening Photos',
+        description: 'Animating a classic movie wallpaper using GSAP',
+        git: 'https://github.com/asitparida/GSAP.PhotoVector.Anim',
+        img: 'assets/project-logos/photoanim.png',
+        href: 'http://gsap-pv-anim-asparida.azurewebsites.net',
+        width: '90%',
+        height: 'auto',
+        bgColor: '$ALIZARIN'
+    },
+    {
+        name: 'Segoe MDL Cheatsheet',
+        description: 'Web crawled SCSS sheet for MDL Icons',
+        git: 'https://github.com/asitparida/SegoeMDLCheatsheet',
+        img: 'assets/project-logos/icons.png',
+        href: 'https://github.com/asitparida/SegoeMDLCheatsheet',
+        width: '150px',
+        height: 'auto',
+        bgColor: '$CARROT'
+    },
+    {
+        name: 'Rich CC',
+        description: 'ng-plugin for showing calendar events',
+        git: 'https://github.com/asitparida/RichCC',
+        img: 'assets/project-logos/richcc.png',
+        href: 'http://richcc.azurewebsites.net',
+        width: '90%',
+        height: 'auto',
+        bgColor: '$NEPHRITIS'
+    },
+    {
+        name: 'Pie Mix',
+        description: 'ng-plugin built using SVG',
+        git: 'https://github.com/asitparida/PieMix',
+        img: 'assets/project-logos/piemix.png',
+        href: 'http://piemix.azurewebsites.net',
+        width: 'auto',
+        height: '70%',
+        bgColor: '$AMETHYST'
+    },
+    {
+        name: 'Vx Grid',
+        description: 'A DOM virtualized ng-plugin',
+        git: 'https://github.com/asitparida/VxGrid',
+        img: 'assets/project-logos/vx-grid.jpg',
+        href: 'http://vxgrid.azurewebsites.net',
+        bgColor: '$ALIZARIN'
+    },
+    {
+        name: 'Tweening Nature',
+        description: 'Animating a wallpaper using GSAP',
+        git: 'https://github.com/asitparida/GSAP.Background.Anim',
+        img: 'assets/project-logos/nature_tween.png',
+        href: 'http://gsap-bganim-asparida.azurewebsites.net',
+        width: '90%',
+        height: 'auto',
+        bgColor: '$PETER_RIVER'
+    },
+    {
+        name: 'Utilities',
+        description: 'quick utilities based on angularjs 1.x',
+        git: 'https://github.com/asitparida?tab=repositories&q=Utility',
+        img: 'assets/project-logos/swiss-knife.png',
+        href: 'https://github.com/asitparida?tab=repositories&q=Utility',
+        width: '60%',
+        height: 'auto',
+        bgColor: '$ALIZARIN'
+    },
+    {
+        name: 'Lazy Panda',
+        description: 'Just too lazy for emotions',
+        git: 'https://github.com/asitparida/Lazy.Panda',
+        img: 'assets/project-logos/panda.png',
+        href: 'http://lazypanda-asparida.azurewebsites.net',
+        width: 'auto',
+        height: '70%',
+        bgColor: '$PETER_RIVER'
+    }
+];
+// tslint:enable:max-line-length
