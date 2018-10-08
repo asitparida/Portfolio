8012bc577f82ff3d831a15b4dcf6e1332c9d8824
Asit Parida <asitparida@live.in>
2018-10-08 18:59:53 -0400

    Reducing the lag of screen

--- Portfolio.Me/Portfolio.Me/portfolio-v2-updated/src/shared/gridScrollFx.js
+++ Portfolio.Me/Portfolio.Me/portfolio-v2-updated/src/shared/gridScrollFx.js
@@ -196,7 +196,7 @@
 			var onScrollFn = function () {
 				if (!self.didScroll) {
 					self.didScroll = true;
-					setTimeout(function () { self._scrollPage(); }, 200);
+					setTimeout(function () { self._scrollPage(); }, 33);
 				}
 
 				if (self.itemsRenderedCount === self.itemsCount) {
